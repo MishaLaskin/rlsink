6735557b544e4cd9bfe2e4223ff45ee7af8397ff
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-07-03 19:08:20 +0000

    update

--- rlsink/simulator/data_collector.py
+++ rlsink/simulator/data_collector.py
@@ -22,30 +22,33 @@ class PointMassDataGenerator:
                                       mode=self.mode,
                                       max_steps=self.path_length)
 
-    def run_simulation(self):
+    def run_simulation(self, output_dir=''):
         obs = self.env.reset()
-        self.data = {k: [] for k in obs}
-        self.data['action'] = []
-        self.data['image_observation'] = []
+        data = {k: [] for k in obs}
+        data['action'] = []
+        data['image_observation'] = []
 
         for i in tqdm(range(self.num_paths)):
 
             obs = self.env.reset()
+
             for j in range(self.path_length):
                 a = self.env.action_space.sample()
                 obs, _, _, _ = self.env.step(a)
-                obs['action'] = a
+
+                obs['action'] = any
                 obs['image_observation'] = self.env.render(32, 32)
                 for k, v in obs.items():
-                    self.data[k].append(v)
+                    data[k].append(v.copy())
 
         file_name = 'point_mass_length' + \
             str(self.path_length) + '_paths_' + str(self.num_paths)
-        np.save(file_name, self.data)
-        self.data = {}
+        np.save(output_dir + file_name, data)
+        print('Saved output in:', output_dir + file_name)
 
 
 if __name__ == "__main__":
-    collector = PointMassDataGenerator(path_length=100, num_paths=2000)
-    collector.run_simulation()
+    output_dir = '/home/misha/downloads/vqvae/data/'
+    collector = PointMassDataGenerator(path_length=100, num_paths=200)
+    collector.run_simulation(output_dir)
     print('done')
