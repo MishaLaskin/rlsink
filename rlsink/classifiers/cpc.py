27cca4da3dd96a39c0718da3c2e53fe91ecd1594
MishaLaskin <laskin.misha@gmail.com>
2019-08-15 19:15:59 -0700

    built sptm graph

--- rlsink/classifiers/cpc.py
+++ rlsink/classifiers/cpc.py
@@ -24,6 +24,8 @@ What we want this file(s) to accomplish:
 Then run this on a dataset to sense check
 """
 
+BATCH_SIZE = 32
+
 
 class CPCSampler(Sampler):
     r"""Samples elements sequentially, always in the same order.
@@ -66,7 +68,7 @@ class CPCSampler(Sampler):
             pos_state = self.data_source.state_data[pos_index][:2]
             neg_state = self.data_source.state_data[index][:2]
             L2 = np.linalg.norm(pos_state-neg_state)
-            if L2 > 0.05:
+            if L2 > 0.1:
                 neg_indices.append(index)
 
         return neg_indices
@@ -87,13 +89,17 @@ class CPCSampler(Sampler):
         return len(self.data_source)
 
 
-
-
-def vqvae_encoder_for_cpc(x, model,device):
+def vqvae_encoder_for_cpc(x, model, device):
 
     x = x.to(device)
     z = model.pre_quantization_conv(model.encoder(x))
     return z
+
+
+def log_sum_exp(arr):
+
+    max_arr = torch.max(arr, 1, keepdim=True).values
+    return max_arr + torch.log(torch.sum(torch.exp(arr - max_arr), 1))
 
 
 class CPC(nn.Module):
@@ -127,6 +133,56 @@ class CPC(nn.Module):
         assert self.batch_size is not None
         assert self.loader is not None
 
+    def encode(self, x):
+        return x
+
+    def loss(self, x, x_next):
+        # Same as density
+        assert x_next.size(0) == x.size(0)
+        z = self.encode(x)
+        z_next = self.encode(x_next)
+        # z = z.unsqueeze(2)  # bs x z_dim x 1
+        #z_next = z_next.unsqueeze(2)
+        w = self.W
+
+        w = w.expand(x.shape[0], w.shape[0], w.shape[1])
+
+        f_out = torch.bmm(torch.bmm(z_next, w), z.permute(0, 2, 1))
+        f_out = f_out.squeeze()
+        f_out /= 1000
+
+        f_pos = f_out[:, :1]
+
+        loss = log_sum_exp(f_out) - torch.log(torch.exp(f_pos))
+
+        return torch.mean(loss), None
+
+    def forward(self, z1, z2):
+        z1 = z1.view(128, 1)
+        z2 = z2.view(-1, 128)
+        return torch.exp(torch.matmul(torch.matmul(z2, self.W), z1).squeeze(0)/1000)
+
+    """
+    def naive_batch_loss(self, z, z_all):
+        # ref sample (32,128,1)
+        z = z.permute(1, 2, 0)
+        # cpc batch samples (32,51,128)
+        z_all = z_all.permute(1, 0, 2)
+        # positive sample (32,1,128)
+        z_pos = z_all[:, :1, :]
+
+        losses = []
+
+        for i in range(BATCH_SIZE):
+            z_i = z[i:i+1, :, :].squeeze(0)
+            z_all_i = z_all[i:i+1, :, :].squeeze(0)
+
+            loss = self.naive_loss(z_i, z_all_i)[0]
+            losses.append(loss)
+
+        # print(torch.stack(losses))
+        return torch.mean(torch.stack(losses)), None
+
     def naive_loss(self, z_t, z_others, verbose=False):
         # maybe try making z
         z_t = z_t.view(128, 1)
@@ -157,7 +213,7 @@ class CPC(nn.Module):
             print('z_others', z_others.shape)
             print('scores shape is:', scores.shape)
 
-        return loss
+        return loss, None
 
     def naive_forward(self, z1, z2):
         z1 = z1.view(128, 1)
@@ -165,81 +221,123 @@ class CPC(nn.Module):
         return torch.exp(torch.matmul(torch.matmul(z2, self.W), z1).squeeze(0)/1000)
 
     def loss(self, z, z_others, verbose=False):
-        batch_size = 32  # z_i.shape[0]
+        batch_size = BATCH_SIZE  # z_i.shape[0]
         cpc_batch_size = 51  # z_j.shape[0]
         z_dim = 128  # z_i.shape[-1]
 
-        z = z.view(batch_size,z_dim,1) #.repeat(1,1,batch_size)
-        z_others = z_others.view(batch_size,cpc_batch_size,z_dim)
-        z_pos = z_others[:,:1,:]
-        z_neg = z_others
-        repeated_w = self.W.unsqueeze(2).repeat(1,1,batch_size).permute(2,0,1)
-        
+        # ref sample (32,128,1)
+        z = z.permute(1, 2, 0)  # .repeat(1,1,batch_size)
+        # cpc batch samples (32,51,128)
+        z_others = z_others.permute(1, 0, 2)
+        # positive sample (32,1,128)
+        z_pos = z_others[:, :1, :]
+
+        repeated_w = self.W.unsqueeze(2).expand(
+            z_dim, z_dim, batch_size).permute(2, 0, 1)
+
+        # z_all.T W z (32,51,128) x (32,128,128) x (32,128,1) = (32,51,1) -> squeeze(2) = (32,51)
+        z_all_W_z = torch.bmm(z_others, torch.bmm(
+            repeated_w, z)).squeeze(2)/1000
+
+        # z_pos.T W z (32,1,128) x (32,128,128) x (32,128,1) = (32,1,1) -> squeeze(2) = (32,1)
+
+        z_pos_W_z = z_all_W_z[:, :1]
+
         def log_sum_exp_batch(arr):
-            max_arr = torch.max(arr,1,keepdim=True).values
-            #max_arr = torch.max(arr)
-            #print(max_arr.shape)
-            #print(torch.log(torch.sum(torch.exp(arr),1)).shape)
-            #print(arr.shape)
-            return max_arr + torch.log(torch.sum(torch.exp(arr - max_arr),1))
+            # arr has shape (batch,cpc_batch) so (32,51) for example
+            # max_arr has shape (32,1)
+            max_arr = torch.max(arr, 1, keepdim=True).values
+            #max_arr = torch.max(arr).unsqueeze(0)
+            # print(max_arr.shape)
+            # print(torch.log(torch.sum(torch.exp(arr),1)).shape)
+            # print(arr.shape)
+            return max_arr + torch.log(torch.sum(torch.exp(arr - max_arr), 1))
 
-        
-        z_pos_W_z = torch.bmm(z_pos,torch.bmm(repeated_w,z)).squeeze(2)
-        z_neg_W_z = torch.bmm(z_neg,torch.bmm(repeated_w,z)).squeeze(2)
-
-        log_exp_f_neg = log_sum_exp_batch(z_neg_W_z)
-        log_exp_f_pos = torch.log(torch.exp(z_pos_W_z).squeeze(1))   
-        loss = torch.mean(log_exp_f_neg - log_exp_f_pos)
-        #print('log_exp_f_neg',torch.mean(log_exp_f_neg))
-        #print('log_exp_f_pos',torch.mean(log_exp_f_pos))
+        # calculate the loss
+        # Loss = - E [log (exp(z_pos.T W z) / sum_i exp( z_i.T W z))]
+        #      = E [log sum_i exp (z_i.T W z) - log (exp (z_pos.T W z ))]
+        log_exp_f_all = log_sum_exp_batch(z_all_W_z).squeeze(1)
+        log_exp_f_pos = torch.log(torch.exp(z_pos_W_z).squeeze(1))
+        loss = torch.mean(log_exp_f_all - log_exp_f_pos)
+        # print('log_exp_f_neg',torch.mean(log_exp_f_neg))
+        # print('log_exp_f_pos',torch.mean(log_exp_f_pos))
         if verbose:
-            print('repeated w',repeated_w.shape)
-            print('z',z.shape)
-            print('z_pos w',z_pos.shape)
-            print('z_neg',z_neg.shape)
-            print('z_pos_W_z',z_pos_W_z.shape)
-            print('z_neg_W_z',z_neg_W_z.shape)
-            print('log_exp_f_neg',log_exp_f_neg.shape)
-            print('loss',loss)
-            
-        return loss
-        
+            print('repeated w', repeated_w.shape)
+            print('z', z.shape)
+            print('z_pos w', z_pos.shape)
+            print('z_neg', z_others.shape)
+            print('z_pos_W_z', z_pos_W_z.shape)
+            print('z_neg_W_z', z_all_W_z.shape)
+            print('log_exp_f_neg', log_exp_f_all.shape)
+            print('loss', loss)
+        info = dict(
+            # z_pos=z,
+            # z_all=torch.mean(z_others),
+            W=torch.mean(self.W),
+            z_pos_W_z=z_pos_W_z,
+            z_all_W_z=torch.mean(z_all_W_z),
+            log_exp_f_pos=log_exp_f_pos,
+            log_exp_f_all=log_exp_f_all,
+
+        )
+        return loss, info
+
     def forward(self, z_i, z_j):
         batch_size = 32  # z_i.shape[0]
         cpc_batch_size = 51  # z_j.shape[0]
         z_dim = 128  # z_i.shape[-1]
 
-        z_i = z_i.view(batch_size,z_dim,1) #.repeat(1,1,batch_size)
-        z_j = z_j.view(batch_size,z_dim,1).permute(0,2,1)
+        z_i = z_i.view(batch_size, z_dim, 1)  # .repeat(1,1,batch_size)
+        z_j = z_j.view(batch_size, z_dim, 1).permute(0, 2, 1)
 
-        repeated_w = self.W.unsqueeze(2).repeat(1,1,batch_size).permute(2,0,1)
-        
-        """ stopped here """
-        score = torch.bmm(z_j,torch.bmm(repeated_w,z_i)).squeeze(2)
-        print('score',score.shape)
-        assert False 
+        repeated_w = self.W.unsqueeze(2).repeat(
+            1, 1, batch_size).permute(2, 0, 1)
+
+        # stopped here 
+        score = torch.bmm(z_j, torch.bmm(repeated_w, z_i)).squeeze(2)
+        print('score', score.shape)
+        assert False
+
+    def encode(self, x):
+        return x
+
+    def log_density(self, x, x_next):
+        # Same as density
+        assert x_next.size(0) == x.size(0)
+        z, _ = self.encode(x)
+        z_next, _ = self.encode(x_next)
+        z = z.unsqueeze(2)  # bs x z_dim x 1
+        z_next = z_next.unsqueeze(2)
+
+        w = self.W
+        w = w.repeat(z.size(0), 1, 1)
+        f_out = torch.bmm(torch.bmm(z_next.permute(0, 2, 1), w), z)
+        f_out = f_out.squeeze()
+
+        return f_out / 1000
+    """
 
 
 def train_cpc():
     n_updates = 100000
-    lr = 3e-2
+    lr = 3e0
     n = 100
     path_length = 100
     T = 10
-    n_neg = 50
-    batch_size = 32
+    n_neg = 8
+    batch_size = BATCH_SIZE
     cpc_batch_size = n_neg+2
-    cpc_model_path = '/home/misha/downloads/rlsink/saved/cpc_weights3.pth'
-    total_samples = batch_size*cpc_batch_size
-    gpu_id = 3
+    cpc_model_path = '/home/misha/research/rlsink/saved/cpc_weights4.pth'
+    total_samples = cpc_batch_size
+    gpu_id = 0
 
     device = torch.device(
-    "cuda:"+str(gpu_id)) if torch.cuda.is_available() else torch.device("cpu")
+        "cuda:"+str(gpu_id)) if torch.cuda.is_available() else torch.device("cpu")
 
     from rlsink.utils.data import load_data_and_data_loaders, load_model
 
-    data_file_path = '/home/misha/downloads/vqvae/data/reacher_no_target_length100_paths_50.npy'
-    model_filename = '/home/misha/downloads/vqvae/results/vqvae_data_reacher_aug11_ne128nd2.pth'
+    data_file_path = '/home/misha/research/vqvae/data/reacher_no_target_length100_paths_2000.npy'
+    model_filename = '/home/misha/research/vqvae/results/vqvae_data_reacher_aug7_ne128nd2.pth'
 
     model, _ = load_model(model_filename)
 
@@ -247,7 +345,7 @@ def train_cpc():
         data_file_path, path_length, shuffle=False, include_state=True)
 
     sampler = CPCSampler(tr_data, path_length=n, proximity_threshold=T,
-                         n_negative=n_neg, batch_size=batch_size)
+                         n_negative=n_neg, batch_size=1)
 
     loader = torch.utils.data.DataLoader(
         tr_data, sampler=sampler, batch_size=total_samples)
@@ -262,28 +360,34 @@ def train_cpc():
 
     optimizer = optim.Adam(cpc.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
-        optimizer, milestones=[20, 40, 60, 80], gamma=0.1)
+        optimizer, milestones=[20, 40, 60], gamma=0.1)
 
     # scheduler =  ReduceLROnPlateau(optimizer, 'min')
 
     for i in range(n_updates):
-        x = next(iter(loader))[0]
-        # print(x.shape)5
-        z = vqvae_encoder_for_cpc(x, model,device)
-        z = z.reshape(cpc_batch_size, batch_size, -1)
-        # print(z.shape)
-        z_t = z[:1]
-        z_others = z[1:]
+        x = torch.cat([next(iter(loader))[0]
+                       for _ in range(batch_size)])  # next(iter(loader))[0]
+        z = vqvae_encoder_for_cpc(x, model, device)
+
+        z = z.view(batch_size, cpc_batch_size, -1)
+
+        z_t = z[:, :1, :]
+        z_all = z[:, 1:, :]
+
         # print(z_t.shape,z_others.shape)
-        loss = cpc.loss(z_t, z_others)
+        loss, info = cpc.loss(z_t, z_all)
 
         loss.backward()
         optimizer.step()
-        if i % 10 == 0:
+        if i % 100 == 0:
+            print('='*20)
             print('epoch', i, 'loss', loss.detach().cpu().item())
+            # for k, v in info.items():
+            #    print(k, v.detach().cpu().numpy())
+            scheduler.step()
 
+        if i % 1000:
             torch.save(cpc.state_dict(), cpc_model_path)
-            #scheduler.step()
 
 
 if __name__ == "__main__":
--- rlsink/graphs/sptm.py
+++ rlsink/graphs/sptm.py
@@ -27,10 +27,190 @@ Ideas:
 """CREATE NODES
 
 1. sample N (e.g. 1000) starting nodes randomly 
-2. traverse dataset sequentially 
+2. traverse dataset sequentially  and encode using vq vae
 2a. If proximal node is in dataset (determined by threshold e.g. 0.05) 
 2aa. merge z into node and update node value with moving average
 2ab. if not, create new node with value z
 
 return list of node values 
 """
+
+from datetime import datetime
+import torch
+import torchvision
+import torch.utils.data
+import random
+from torch.utils.data.sampler import Sampler
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import networkx as nx
+from rlsink.classifiers.cpc import CPC, CPCSampler
+device = torch.device(
+    "cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+def vqvae_encoder_as_numpy(x, model):
+    x = x.to(device)
+    z = model.pre_quantization_conv(model.encoder(x))
+    return z.detach().cpu().numpy()
+
+
+def L2(x1, x2):
+    return np.linalg.norm(x1-x2)
+
+
+def polyak_avg(x1, x2, beta=0.95):
+    return beta * x1 + (1-beta)*x2
+
+
+"""
+Create nodes only (no edges)
+"""
+
+
+def create_node_list(data,
+                     vqvae_model,
+                     threshold=0.1,
+                     n_starting_nodes=1,
+                     batch_size=100,
+                     z_dim=128
+                     ):
+    loader = torch.utils.data.DataLoader(
+        data, shuffle=False, batch_size=batch_size)
+    random_loader = torch.utils.data.DataLoader(
+        data, shuffle=True, batch_size=n_starting_nodes)
+
+    init_images = next(iter(random_loader))[0]
+    nodes = vqvae_encoder_as_numpy(init_images, vqvae_model)
+
+    for i, batch in enumerate(iter(loader)):
+        print(i, end='\r')
+        images = batch[0]
+        latents = vqvae_encoder_as_numpy(images, vqvae_model)
+        for z in latents:
+            # find closest node in graph
+            distances = np.linalg.norm(
+                nodes.reshape(-1, 128)-z.reshape(-1), axis=1)
+            min_idx = np.argmin(distances)
+            min_dist = distances[min_idx]
+
+            if min_dist < threshold:
+                nodes[min_idx] = polyak_avg(nodes[min_idx], z)
+            else:
+                new_node = np.expand_dims(z, axis=0)
+                nodes = np.concatenate((nodes, new_node), axis=0)
+
+    return nodes
+
+
+"""
+Build edges with retrieval network
+"""
+
+
+def build_edges_with_cpc(
+        nodes,
+        data,
+        z_dim=128,
+        path_length=100,
+        proximity_threshold=10,
+        n_negative=10,
+        path='/home/misha/research/rlsink/saved/cpc_weights4.pth'):
+
+    batch_size = 1
+    cpc_batch_size = n_negative+2
+    total_samples = batch_size*cpc_batch_size
+
+    sampler = CPCSampler(data, path_length=path_length,
+                         proximity_threshold=proximity_threshold, n_negative=n_negative, batch_size=1)
+
+    loader = torch.utils.data.DataLoader(
+        data, sampler=sampler, batch_size=total_samples)
+
+    cpc_model = CPC(z_dim=z_dim,
+                    cpc_batch=cpc_batch_size,
+                    batch_size=1,
+                    loader=loader).cuda()
+
+    cpc_model.load_state_dict(torch.load(path))
+    cpc_model.eval()
+
+    nodes = torch.tensor(nodes).float().to(device)
+    scores = []
+    for i, n in enumerate(nodes):
+        node_scores = cpc_model.forward(n, nodes)
+        scores.append(node_scores)
+    scores = torch.cat(scores, 1)
+    return scores
+
+
+def preprocess_scores(scores):
+    clean_scores = torch.log(scores).detach().cpu()  # torch.log
+    clean_scores = torch.exp(-clean_scores)
+    np_scores = clean_scores.detach().cpu().numpy()
+    return np_scores
+
+
+def remove_false_negatives(np_scores, np_nodes):
+    print('removing false negatives')
+    np_scores.shape
+    n = np_scores.shape[0]
+    # print(nodes.shape)
+    #assert False
+    #np_nodes = nodes.detach().cpu().numpy()
+    for i in range(n):
+        print(i, end='\r')
+        for j in range(n):
+
+            if np.linalg.norm(np_nodes[i]-np_nodes[j]) > .2:
+                np_scores[i, j] = float("inf")
+    return np_scores
+
+
+def build_sptm_graph(data, model):
+    nodes = create_node_list(data,
+                             model,
+                             threshold=0.15,
+                             n_starting_nodes=1,
+                             batch_size=100,
+                             z_dim=128)
+
+    scores = build_edges_with_cpc(nodes, data)
+    np_scores = preprocess_scores(scores)
+    np_scores = remove_false_negatives(np_scores, nodes)
+
+    G = nx.Graph()
+    n = len(nodes)
+    for i in range(n):
+        for j in range(n):
+            G.add_edge(i, j, weight=np_scores[i, j])
+
+    return G, nodes
+
+
+if __name__ == "__main__":
+    from rlsink.utils.data import load_data, load_data_and_data_loaders, load_model
+    import pickle
+
+    def save_object(obj, filename):
+        with open(filename, 'wb') as output:  # Overwrites any existing file.
+            pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+    data_file_path = '/home/misha/research/vqvae/data/reacher_no_target_length100_paths_2000.npy'
+    model_filename = '/home/misha/research/vqvae/results/vqvae_data_reacher_aug7_ne128nd2.pth'
+
+    model, vqvae_data = load_model(model_filename)
+    data, _, _, _ = load_data_and_data_loaders(
+        data_file_path, 100, shuffle=False, include_state=True)
+    # data = load_data(
+    #    data_file_path, include_state=False)
+    G, nodes = build_sptm_graph(data, model)
+    now = datetime.now()
+
+    dt_string = ""  # now.strftime("%m/%d/%Y %H:%M")
+    filename = '/home/misha/research/rlsink/saved/reacher_graph.pkl'
+
+    obj = {'graph': G, 'nodes': nodes}
+    #filehandler = open(filename + '_'+dt_string+'.obj', 'w')
+    save_object(obj, filename)
